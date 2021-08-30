8e0458ff9dab1a0a1a7af926d5a0d07db0b0ed5b
eub456 <eub456@gmail.com>
2021-08-30 12:30:10 +0900

    hello

---
+++ README (2).md
@@ -0,0 +1,2 @@
+# demo-spring-boot-web
+spring boot를 사용해 매우 간단한 웹 에플리케이션을 실행하는 예제입니다
---
+++ src/main/java/com/hyeonguj/demo/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.hyeonguj.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping
+    public String home() {
+        return "welcome";
+    }
+}
